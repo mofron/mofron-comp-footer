6b3921430676dfeacacb8692f2ef5a06101691bc
simpart <simpart>
2020-07-15 04:27:15 +0000

    supported v1.1

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 /**
  * @file   mofron-comp-footer/index.js
  * @brief  footer component for mofron
- * @author simpart
+ * @license MIT
  */
-const mf     = require('mofron');
 const Border = require('mofron-effect-border');
 const Vrtpos = require('mofron-effect-vrtpos');
-/**
- * @class mofron.comp.Footer
- * @brief footer component for mofron
- */
-mf.comp.Footer = class extends mf.Component {
-    
+const cmputl = mofron.util.component;
+
+module.exports = class extends mofron.class.Component {
     /**
      * initialize component
      * 
-     * @param po paramter or option
+     * @param (dict) component config
+     * @type private
      */
-    constructor (po) {
+    constructor (p1) {
         try {
             super();
             this.name('Footer');
-            this.prmOpt(po);
+            this.config(p1);
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -31,38 +28,60 @@ mf.comp.Footer = class extends mf.Component {
     /**
      * initialize dom contents
      * 
-     * @param prm : 
+     * @type private
      */
-    initDomConts (prm) {
+    initDomConts () {
         try {
             super.initDomConts();
             this.size('100%', '2rem');
-            
-            this.effect([
-                new Border({
-                    type  : 'top',
-                    color : [128, 128, 128]
-                })
-            ]);
+            this.effect(
+	        new Border({ position: 'top', color: [128, 128, 128], tag: "Footer" })
+            );
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    mainColor (prm) {
+    /**
+     * background color setter/getter
+     * 
+     * @param (mixed) (mixed (color)) string: color name, #hex
+     *                array: [red, green, blue, (alpha)]
+     * @param (dict) style option
+     * @return (mixed) string: text color
+     *                 null: not set
+     * @type parameter
+     */
+    mainColor (prm, opt) {
         try {
-            if (undefined === prm) {
-                /* getter */
-                return mf.func.getColor(this.style('border-top-color'));
-            }
-            /* setter */
-            this.effect('Border').color(prm);
+            return this.baseColor(prm,opt);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
+    }
+    
+    /**
+     * border color setter/getter
+     * 
+     * @param (mixed) (mixed (color)) string: color name, #hex
+     *                array: [red, green, blue, (alpha)]
+     * @param (dict) style option
+     * @return (mixed) string: text color
+     *                 null: not set
+     * @type parameter
+     */
+    accentColor (prm, opt) {
+        try {
+	    if (undefined !== prm) {
+                this.effect({ name: "Border", tag: "Footer" }).color(prm);
+	    }
+	    return cmputl.color(this, "border-top", prm, opt);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 }
-module.exports = mofron.comp.Footer;
 /* end of file */
